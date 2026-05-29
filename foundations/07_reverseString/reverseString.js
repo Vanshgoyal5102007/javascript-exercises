803d5175bc85ae93df0e5b46ba4cc84fe8776d38
AmberVolt <184669028+Vanshgoyal5102007@users.noreply.github.com>
2026-05-29 14:13:49 +0530

    07-reverseString: Complete exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = ""
+    for(let i = -1; i >= -(string.length); i--){
+        result += string.at(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
